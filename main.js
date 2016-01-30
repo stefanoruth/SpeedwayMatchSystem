ef136c338e3a7d255d55f482e8dfa50b0c426f38
stefanoruth <stefano6262@gmail.com>
2016-01-31 00:54:36 +0100

    more frontend

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
 var RaceMatch = require('./lib/RaceMatch.js');
 var RaceDriver = require('./lib/RaceDriver.js');
+var Vue = require('vue');
 
-var app = new Vue({
+var myApp = new Vue({
 
 	el: '#app',
 
 	data: {
+		heats: [],
+	},
 
+	ready: function() {
+		this.calcRace();
+
+		this.shuffleHeats();
+	},
+
+	methods: {
+
+		calcRace: function() {
+			var race = new RaceMatch()
+			this.heats = race.setDrivers(12).race();
+
+			
+		},
+
+		shuffleHeats: function(){
+			function Shuffle(o) {
+				for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+				return o;
+			};
+
+			this.heats = Shuffle(this.heats);
+		},
 	},
 });
