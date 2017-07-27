996e7fbd9a3a82d18f6a9d337e97f99f764de19f
Stefano Ruth <stefano6262@gmail.com>
2017-07-27 14:22:12 +0200

    Change default value to empty string insted of null

--- src/RaceDriver.js
+++ src/RaceDriver.js
@@ -1,12 +1,12 @@
 module.exports = function(number, name, group, club) {
-	this.number = (number = typeof number !== 'undefined' ? number : null);
-	this.name = (name = typeof name !== 'undefined' ? name : null);
-	this.group = (group = typeof group !== 'undefined' ? group : null);
-	this.club = (club = typeof club !== 'undefined' ? club : null)
+	this.number = (number = typeof number !== 'undefined' ? number : '');
+	this.name = (name = typeof name !== 'undefined' ? name : '');
+	this.group = (group = typeof group !== 'undefined' ? group : '');
+	this.club = (club = typeof club !== 'undefined' ? club : '')
 	this.id = null;
 
 	this.isset = function(){
-		if (this.number != null || this.name != null || this.group != null || this.club != null) {
+		if (this.number != '' || this.name != '' || this.group != '' || this.club != '') {
 			return true;
 		}
 		return false;
